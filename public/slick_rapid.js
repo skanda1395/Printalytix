425f7bb0ee18de73c2ef7c45c6156100a17ca8b1
skanda1395 <skanda1395@gmail.com>
2021-05-25 14:50:20 +0530

    added videos to the top

---
+++ public/slick_rapid.js
@@ -0,0 +1,31 @@
+$(".rapid_ptg_videos").slick({
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
